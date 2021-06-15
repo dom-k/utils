2a53c3ab4bd0784c9f50e946ad7c2cf5fb00b2fe
Dominik K <kuhbachdominik@gmail.com>
2021-06-15 21:32:35 +0200

    cleanup old stuff, add new scripts

--- remove-disabled-snaps.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-set -eu
-
-snap list --all | awk '/disabled/{print $1, $3}' |
-    while read snapname revision; do
-        snap remove "$snapname" --revision="$revision"
-    done
